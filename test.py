dbf161fb1acb14d69b02a2fc4b0997c81f02888b
Mohan Kumar Paluru <46961568+mohankumarpaluru@users.noreply.github.com>
2023-05-31 19:21:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,31 @@
-import os
+from pathlib import Path
 import pyarrow.parquet as pq
 import pandas as pd
 
 # Specify the path to the level1 folder
-level1_folder = '/path/to/level1/'
+level1_folder = Path('/path/to/level1/')
 
 # Iterate through each level2 folder
-for level2_folder in os.listdir(level1_folder):
-    level2_folder_path = os.path.join(level1_folder, level2_folder)
-    
-    # Check if it's a directory
-    if os.path.isdir(level2_folder_path):
-        # Create an empty DataFrame to hold the combined data
-        combined_df = pd.DataFrame()
-        
+for level2_folder in level1_folder.iterdir():
+    if level2_folder.is_dir():
+        dfs = []
+
         # Iterate through each Parquet file in the level2 folder
-        for file in os.listdir(level2_folder_path):
-            if file.endswith('.parquet'):
-                file_path = os.path.join(level2_folder_path, file)
-                
-                # Read the Parquet file into a PyArrow table
-                table = pq.read_table(file_path)
-                
-                # Convert the table to a Pandas DataFrame
-                df = table.to_pandas()
-                
-                # Append the DataFrame to the combined DataFrame
-                combined_df = combined_df.append(df)
-        
+        for file_path in level2_folder.glob('*.parquet'):
+            # Read the Parquet file into a PyArrow table
+            table = pq.read_table(file_path)
+
+            # Convert the table to a Pandas DataFrame
+            df = table.to_pandas()
+
+            # Add the DataFrame to the list
+            dfs.append(df)
+
+        # Concatenate all DataFrames in the list
+        combined_df = pd.concat(dfs, ignore_index=True)
+
         # Generate the output CSV file path
-        output_csv_path = os.path.join(level1_folder, f'{level2_folder}.csv')
-        
+        output_csv_path = level1_folder / f'{level2_folder.stem}.csv'
+
         # Save the combined DataFrame as a CSV file
         combined_df.to_csv(output_csv_path, index=False)
