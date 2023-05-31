8183e47a13a02798695e9d6c46707acf42c72478
Mohan Kumar Paluru <46961568+mohankumarpaluru@users.noreply.github.com>
2023-05-31 18:52:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,34 @@ import os
 import pyarrow.parquet as pq
 import pandas as pd
 
-input_folder = '/path/to/parquet_files/'
-output_folder = '/path/to/csv_files/'
+# Specify the path to the level1 folder
+level1_folder = '/path/to/level1/'
 
-for filename in os.listdir(input_folder):
-    if filename.endswith('.parquet'):
-        file_path = os.path.join(input_folder, filename)
-        output_file = os.path.splitext(filename)[0] + '.csv'
-        output_path = os.path.join(output_folder, output_file)
-
-        table = pq.read_table(file_path)
-        df = table.to_pandas()
-        df.to_csv(output_path, index=False)
+# Iterate through each level2 folder
+for level2_folder in os.listdir(level1_folder):
+    level2_folder_path = os.path.join(level1_folder, level2_folder)
+    
+    # Check if it's a directory
+    if os.path.isdir(level2_folder_path):
+        # Create an empty DataFrame to hold the combined data
+        combined_df = pd.DataFrame()
+        
+        # Iterate through each Parquet file in the level2 folder
+        for file in os.listdir(level2_folder_path):
+            if file.endswith('.parquet'):
+                file_path = os.path.join(level2_folder_path, file)
+                
+                # Read the Parquet file into a PyArrow table
+                table = pq.read_table(file_path)
+                
+                # Convert the table to a Pandas DataFrame
+                df = table.to_pandas()
+                
+                # Append the DataFrame to the combined DataFrame
+                combined_df = combined_df.append(df)
+        
+        # Generate the output CSV file path
+        output_csv_path = os.path.join(level1_folder, f'{level2_folder}.csv')
+        
+        # Save the combined DataFrame as a CSV file
+        combined_df.to_csv(output_csv_path, index=False)
